59c517974da9488aaffef7d5beb5e1bef2eef153
KiteCrown <162701747+KiteCrown@users.noreply.github.com>
2025-01-19 14:28:37 +0800

    new levels but i can't figure out how to make the enemies in the old tilemap disappear.

--- main.ts
+++ main.ts
@@ -4,21 +4,74 @@ namespace SpriteKind {
 sprites.onOverlap(SpriteKind.round, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.follow(mySprite)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    level += 1
+    if (level == 0) {
+        tiles.loadMap(tiles.createSmallMap(tilemap`level4`))
+    } else if (level == 1) {
+        tiles.loadMap(tiles.createSmallMap(tilemap`level11`))
+    } else if (level == 2) {
+    	
+    } else if (level == 3) {
+    	
+    } else if (level == 4) {
+    	
+    } else if (level == 5) {
+    	
+    } else if (level == 6) {
+    	
+    } else if (level == 7) {
+    	
+    } else if (level == 8) {
+    	
+    } else if (level == 9) {
+    	
+    }
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile5`)
+    for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
+        monster = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        tiles.placeOnTile(monster, value)
+        tiles.setTileAt(value, assets.tile`myTile`)
+        monster.setImage(monsterImageList._pickRandom())
+    }
+})
+function tilemaps (level: Sprite) {
+	
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     lantern.stopLanternEffect()
     controller.moveSprite(mySprite, 0, 0)
     extraEffects.createSpreadEffectOnAnchor(mySprite, extraEffects.createSingleColorSpreadEffectData(2, ExtraEffectPresetShape.Explosion), 5000, 100)
     scene.cameraShake(8, 5000)
     sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
-    sprites.destroy(mySprite)
+    mySprite.setFlag(SpriteFlag.Invisible, true)
     timer.after(6000, function () {
         game.gameOver(false)
     })
 })
+let level = 0
 let monster: Sprite = null
 let mySprite: Sprite = null
+let monsterImageList: Image[] = []
 tiles.loadMap(tiles.createSmallMap(tilemap`level4`))
-let monsterImageList = [
+monsterImageList = [
 img`
     . 1 . . . . 1 . 
     . 1 1 1 1 1 1 . 
@@ -70,6 +123,9 @@ mySprite = sprites.create(img`
     . . c c b c . . 
     . . e . . e . . 
     `, SpriteKind.Player)
+lantern.startLanternEffect(mySprite)
+lantern.setLightBandWidth(7)
+tiles.placeOnRandomTile(mySprite, assets.tile`myTile5`)
 let round2 = sprites.create(img`
     ...........bbbbbbbbbbb...........
     ........bbbbbbbbbbbbbbbbb........
@@ -107,8 +163,6 @@ let round2 = sprites.create(img`
     `, SpriteKind.round)
 round2.setFlag(SpriteFlag.Invisible, true)
 controller.moveSprite(mySprite)
-lantern.startLanternEffect(mySprite)
-lantern.setLightBandWidth(7)
 scene.cameraFollowSprite(mySprite)
 for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
     monster = sprites.create(img`
